93c3b083c624a806566e8f60c17bfe45233f2c49
nofacez <genizzz@yandex.ru>
2021-03-08 19:19:53 +0300

    project setup #5 + bootstrap + html template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-const params = {
-  hp: 'low',
-  mana: 'full',
+// import _ from 'lodash';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import onChange from 'on-change';
+import axios from 'axios';
+
+// btn.addEventListener('click', () => console.log('click'));
+
+const state = {
+  form: {
+    state: 'initial',
+    value: '',
+  },
 };
 
-console.log(params);
+const watchedState = onChange(state, (path, value, prevValue, name) => {
+  if (path === 'state.form.state') {
+    if (value === 'filled') {
+      axios.get(value).then((data) => console.log(data));
+    }
+  }
+  console.log(path, value);
+});
+
+// console.log(state);
+
+const btn = document.getElementById('button');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const url = state.form.value;
+  console.log(url);
+  watchedState.form.state = 'filled';
+});
+
+const input = document.querySelector('input');
+input.addEventListener('input', (e) => {
+  watchedState.form.value = e.target.value;
+  // console.log(e.target.value);
+  // console.log(state);
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+
+};
