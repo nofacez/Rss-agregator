2db9b1e54e6757748b412d7b855f749e91511059
nofacez <genizzz@yandex.ru>
2021-04-02 21:09:42 +0300

    final test fix #3.3

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,13 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //   },
+      // },
     ],
   },
 
