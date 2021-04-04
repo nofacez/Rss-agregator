5ecbfd1b97b8ac36bf3513bd3c6c0be41c521f5b
nofacez <genizzz@yandex.ru>
2021-04-04 19:30:59 +0300

    removing promises #1.1

--- dist/main.js
+++ dist/main.js
@@ -49580,22 +49580,6 @@ __webpack_require__.r(__webpack_exports__);
 
 const formatUrl = (url) => `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`;
 
-// const getNewPosts = (state, renderPosts) => {
-//   const oldPostsLinks = state.rss.posts.map(({ link }) => link);
-//   state.form.feedList.forEach((url) => {
-//     axios.get(formatUrl(url))
-//       .then((response) => {
-//         const rssContent = response.data.contents;
-//         const { posts } = parseRss(rssContent);
-//         const newPosts = posts
-//           .map((item) => ({ ...item }))
-//           .filter(({ link }) => !_.includes(oldPostsLinks, link));
-//         newPosts.forEach((post) => state.rss.posts.unshift(post));
-//         renderPosts(state);
-//       });
-//   });
-// };
-
 const getNewPosts = (state, renderPosts) => {
   const oldPostsLinks = state.rss.posts.map(({ link }) => link);
   state.form.feedList.forEach(async (url) => {
@@ -49625,38 +49609,9 @@ const start = (state) => {
   const watchedState = on_change__WEBPACK_IMPORTED_MODULE_1___default()(state, (path) => (0,_view_js__WEBPACK_IMPORTED_MODULE_5__.default)(watchedState, path, timeoutCheckForNewPosts));
 
   addRssButton.addEventListener('click', async (e) => {
-    console.log(watchedState);
     e.preventDefault();
     const url = watchedState.form.value;
-    //  URL validation
     watchedState.form.status = 'checking';
-    // schema.validate(url).then(() => {
-    //   if (watchedState.form.feedList.includes(url)) {
-    //     watchedState.form.status = 'alreadyAddedRss';
-    //   } else if (watchedState.form.value.length === 0) {
-    //     watchedState.form.status = 'unfilled';
-    //   } else {
-    //     axios.get(formatUrl(url))
-    //       .then((response) => {
-    //         const rssContent = response.data.contents;
-    //         const { status, feed, posts } = parseRss(rssContent, url);
-    //         if (status === 'success') {
-    //           watchedState.form.feedList.unshift(url);
-    //           watchedState.rss.feeds.push({ ...feed });
-    //           const previousPosts = watchedState.rss.posts;
-    //           watchedState.rss.posts = [...posts, ...previousPosts];
-    //           watchedState.form.value = '';
-    //         }
-    //         watchedState.form.status = status;
-    //       })
-    //       .catch(() => {
-    //         watchedState.form.status = 'networkProblems';
-    //       });
-    //   }
-    // })
-    //   .catch(() => {
-    //     watchedState.form.status = 'invalidUrl';
-    //   });
     if (watchedState.form.feedList.includes(url)) {
       watchedState.form.status = 'alreadyAddedRss';
     } else if (watchedState.form.value.length === 0) {
--- src/rssAgregator.js
+++ src/rssAgregator.js
@@ -9,22 +9,6 @@ import render from './view.js';
 
 const formatUrl = (url) => `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`;
 
-// const getNewPosts = (state, renderPosts) => {
-//   const oldPostsLinks = state.rss.posts.map(({ link }) => link);
-//   state.form.feedList.forEach((url) => {
-//     axios.get(formatUrl(url))
-//       .then((response) => {
-//         const rssContent = response.data.contents;
-//         const { posts } = parseRss(rssContent);
-//         const newPosts = posts
-//           .map((item) => ({ ...item }))
-//           .filter(({ link }) => !_.includes(oldPostsLinks, link));
-//         newPosts.forEach((post) => state.rss.posts.unshift(post));
-//         renderPosts(state);
-//       });
-//   });
-// };
-
 const getNewPosts = (state, renderPosts) => {
   const oldPostsLinks = state.rss.posts.map(({ link }) => link);
   state.form.feedList.forEach(async (url) => {
@@ -56,35 +40,7 @@ const start = (state) => {
   addRssButton.addEventListener('click', async (e) => {
     e.preventDefault();
     const url = watchedState.form.value;
-    //  URL validation
     watchedState.form.status = 'checking';
-    // schema.validate(url).then(() => {
-    //   if (watchedState.form.feedList.includes(url)) {
-    //     watchedState.form.status = 'alreadyAddedRss';
-    //   } else if (watchedState.form.value.length === 0) {
-    //     watchedState.form.status = 'unfilled';
-    //   } else {
-    //     axios.get(formatUrl(url))
-    //       .then((response) => {
-    //         const rssContent = response.data.contents;
-    //         const { status, feed, posts } = parseRss(rssContent, url);
-    //         if (status === 'success') {
-    //           watchedState.form.feedList.unshift(url);
-    //           watchedState.rss.feeds.push({ ...feed });
-    //           const previousPosts = watchedState.rss.posts;
-    //           watchedState.rss.posts = [...posts, ...previousPosts];
-    //           watchedState.form.value = '';
-    //         }
-    //         watchedState.form.status = status;
-    //       })
-    //       .catch(() => {
-    //         watchedState.form.status = 'networkProblems';
-    //       });
-    //   }
-    // })
-    //   .catch(() => {
-    //     watchedState.form.status = 'invalidUrl';
-    //   });
     if (watchedState.form.feedList.includes(url)) {
       watchedState.form.status = 'alreadyAddedRss';
     } else if (watchedState.form.value.length === 0) {
